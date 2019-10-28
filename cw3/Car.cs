ba1219800565a60683b8adf09cb30e0292b3ece0
Bartek <Bartek@DESKTOP-NC7V43R>
2019-10-28 23:39:45 +0100

    Dodaj pliki projektów.

---
+++ cw3/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cw3
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        static int iloscKol;
+        Car()
+        {
+
+        }
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+        public static Car Create(double pojemnoscSilnika, string marka)
+        {
+
+            Car car = new Car(pojemnoscSilnika, marka);
+
+            return car;
+        }
+        static Car() 
+        {
+            iloscKol = 4;
+
+        }
+        ~Car()
+        {
+            MessageBox.Show("Zwalniam pamiec");
+        }
+    }
+}
---
+++ cw3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car(1.0, "mercedes");
+            Car car2 = new Car(2.5, "opel");
+            Car.Create(2.5, "opel");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
