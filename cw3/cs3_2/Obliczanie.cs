a5d35193c512a01bcadce658ee366fe1797ef27b
Bartosz <44466149+Cwiekson@users.noreply.github.com>
2019-10-29 12:37:05 +0100

    Add files via upload

---
+++ cw3/cs3_2/Obliczanie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs3_2
+{
+     public class Obliczanie
+    {
+        public double wynik;
+        public double a;
+        public double b;
+
+        public  double Dodowanie(double a,double b)
+        {
+           return wynik = a + b;
+        }
+        public  double Odejmowanie(double a, double b)
+        {
+            return wynik = a - b;
+        }
+        public double Mnozenie(double a, double b)
+        {
+           return wynik = a * b;
+        }
+        public  double Dzielenie(double a, double b)
+        {
+            try
+            {
+                wynik = a / b;
+            }
+            
+            catch
+            {
+                Console.WriteLine("NIe mozna dzielic przez 0");
+                
+            }
+            return wynik;
+        }
+        public double POtegowanie(double a,double b)
+        {
+            return wynik = Math.Pow(a, b);
+        }
+        public  double Pierwiastkowanie(double a)
+        {
+            return wynik = Math.Sqrt(a);
+        }
+
+    }
+}
---
+++ cw3/cs3_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] table = new int[2];
+            //try
+            //{
+            //    table[25] = 2;
+            //    throw new IndexOutOfRangeException();
+
+            //}
+            //catch (IndexOutOfRangeException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Blad");
+            //}
+            //Console.ReadKey();
+           Obliczanie wynik1 = new Obliczanie();
+            wynik1.a = 4.0;
+            wynik1.b = 2.0;
+
+            
+
+            Console.WriteLine(wynik1.Pierwiastkowanie(wynik1.a));
+            Console.ReadKey();
+        }
+    }
+}
